090c362a91293dadeaf2b3ef372c70b0c6883779
isafwan <isafwan@outlook.com>
2020-05-26 04:28:27 -0400

    added beans package and Product class

---
+++ src/main/java/come/test/hplus/beans/Product.java
@@ -0,0 +1,38 @@
+package come.test.hplus.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+
+    @Id
+    private int id;
+    private String name;
+    private String imagePath;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
