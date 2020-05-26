c4964babc9bbbebcd65d8f52e2450e2b04389efb
isafwan <isafwan@outlook.com>
2020-05-26 09:16:39 -0400

    attached bindingResult param to method

--- src/main/java/come/test/hplus/controllers/RegisstrationController.java
+++ src/main/java/come/test/hplus/controllers/RegisstrationController.java
@@ -6,6 +6,7 @@ import come.test.hplus.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,7 +20,7 @@ public class RegisstrationController {
         private UserRepository userRepository;
 
     @PostMapping("/registeruser")
-    public String registerUser(@Valid @ModelAttribute("newuser") User user, Model model){
+    public String registerUser(@Valid @ModelAttribute("newuser") User user, BindingResult result,  Model model){
         System.out.println("in registration controller");
         userRepository.save(user);
         model.addAttribute("dataSaved","User registered successfully");
