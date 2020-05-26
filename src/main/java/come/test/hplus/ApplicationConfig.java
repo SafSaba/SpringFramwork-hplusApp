7867feebea2130ed14c9e7f12006a1f77def07e3
isafwan <isafwan@outlook.com>
2020-05-26 00:30:44 -0400

    Map static resources in the context config class & view resolver in context config for JSPs

--- src/main/java/come/test/hplus/ApplicationConfig.java
+++ src/main/java/come/test/hplus/ApplicationConfig.java
@@ -1,4 +1,32 @@
 package come.test.hplus;
 
-public class ApplicationConfig {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan(basePackages = "com.test.hplus")
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("css/**", "images/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/images");
+    }
+
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("WEB-INF/jps");
+        viewResolver.setPrefix(".jsp");
+        viewResolver.setViewClass((JstlView.class));
+        return viewResolver;
+    }
 }
+
