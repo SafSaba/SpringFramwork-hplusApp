1cd77527af455e5ef368574e0c81a790d6a92c70
isafwan <isafwan@outlook.com>
2020-05-27 03:59:17 -0400

    added method to search by name with param name

--- src/main/java/come/test/hplus/controllers/LoginController.java
+++ src/main/java/come/test/hplus/controllers/LoginController.java
@@ -2,6 +2,9 @@ package come.test.hplus.controllers;
 
 
 import come.test.hplus.beans.Login;
+import come.test.hplus.beans.User;
+import come.test.hplus.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginController {
 
-
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/login")
     public String login(@ModelAttribute("login")Login login){
+      User user = userRepository.searchByName(login.getUsername());
+
         return "login";
     }
 }
