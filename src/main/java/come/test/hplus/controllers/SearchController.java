c3e4e17ddee3a0828f44cd3ea78e99cdbf59c074
isafwan <isafwan@outlook.com>
2020-05-28 03:10:05 -0400

    Return a DeferredResult<String> from controller and handle AsyncRequestTimeoutException.class in AppExcHandler

--- src/main/java/come/test/hplus/controllers/SearchController.java
+++ src/main/java/come/test/hplus/controllers/SearchController.java
@@ -4,10 +4,12 @@ package come.test.hplus.controllers;
 import come.test.hplus.beans.Product;
 import come.test.hplus.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,15 +24,19 @@ public class SearchController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private AsyncTaskExecutor executor;
 
     @GetMapping("/search")
-    public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+    public DeferredResult<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+        DeferredResult deferredResult = new DeferredResult();
+
         System.out.println("in search controller");
         System.out.println("search criteria : " + search);
         System.out.println("Async supported in application: "+request.isAsyncSupported());
         System.out.println("Thread from the servlet container : " + Thread.currentThread().getName());
 
-        return () -> {
+       /*return () -> {
             Thread.sleep(3000);
             System.out.println("Thread from the spring mvc task executor : " + Thread.currentThread().getName());
             List<Product> products = new ArrayList<>();
@@ -39,7 +45,21 @@ public class SearchController {
 
             return "search";
 
-        };
+        };*/
+
+        executor.execute(()->{
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread from the spring mvc task executor : " + Thread.currentThread().getName());
+            List<Product> products = new ArrayList<>();
+            products = productRepository.searchByName(search);
+            model.addAttribute("products", products);
+            deferredResult.setResult("search");
+        });
+        return deferredResult;
     }
 
 //    @GetMapping
