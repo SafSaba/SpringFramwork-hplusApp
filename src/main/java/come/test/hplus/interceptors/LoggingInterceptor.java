83b618510dcf42e5d054a6946acdd10424dec91c
isafwan <isafwan@outlook.com>
2020-05-29 02:29:55 -0400

    added preHandle method to logging Intereptor class

---
+++ src/main/java/come/test/hplus/interceptors/LoggingInterceptor.java
@@ -0,0 +1,31 @@
+package come.test.hplus.interceptors;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String seesionId = null;
+
+        if (null != request.getCookies()) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("JSESSIONID".equals(cookie.getName())) {
+                    seesionId = cookie.getValue();
+                }
+            }
+        }
+
+        System.out.println("Incoming request data log: Session : " + seesionId +
+                " at" + new Date() + " for " + request.getRequestURI());
+
+        return true;
+    }
+
+}
